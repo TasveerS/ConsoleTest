b3965af7e2c512b006752e2e800389aea6686cdb
TasveerS <tasveersingh@hotmail.com>
2019-01-24 12:19:07 +0200

    Add project files.

---
+++ TryToDie/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryToDie
+{
+   public abstract class Products
+    {
+         
+        public string Name { get; set; }
+        public string Size { get; set; }
+
+        public abstract double CalcDefaultCost();
+    }
+}
---
+++ TryToDie/Shirt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryToDie
+{
+    class Shirt : Products
+    {
+        public override double CalcDefaultCost()
+        {
+            double price = 0.0;
+            if (Size == "S")
+            {
+                price = 10;
+            }
+
+            else if (Size == "M")
+            {
+                price = 20;
+            }
+
+            else if (Size == "L")
+            {
+                price = 30;
+            }
+
+            return price;
+        }
+    }
+}
